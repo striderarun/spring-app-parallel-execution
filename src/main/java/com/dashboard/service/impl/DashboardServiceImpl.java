53d53d6d6af0afd6ae6223b542206ecf0a23ae01
strider90arun <strider90arun@gmail.com>
2017-07-06 11:08:17 -0700

    Remving unused imports

--- src/main/java/com/dashboard/service/impl/DashboardServiceImpl.java
+++ src/main/java/com/dashboard/service/impl/DashboardServiceImpl.java
@@ -1,10 +1,6 @@
 package com.dashboard.service.impl;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -14,15 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.dashboard.beans.CompensationDetailsBean;
 import com.dashboard.beans.EmployeeDashboardBean;
-import com.dashboard.beans.InsuranceDetailsBean;
-import com.dashboard.beans.PersonalDetailsBean;
-import com.dashboard.beans.RoleDetailsBean;
-import com.dashboard.beans.SkillDetailsBean;
-import com.dashboard.beans.UnitDetailsBean;
-import com.dashboard.parallel.ParallelProcessor;
-import com.dashboard.parallel.Signature;
 import com.dashboard.service.DashboardService;
 import com.dashboard.service.EmployeeService;
 import com.dashboard.service.InsuranceService;
